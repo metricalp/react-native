52762bf037d836996523ef0a10a7fab1b7ab4428
MetinArslanturk <metinarslanturkk@gmail.com>
2024-03-01 00:41:12 +0300

    build script fix

--- build.js
+++ build.js
@@ -5,8 +5,6 @@ const entryFile = 'src/index.ts';
 const shared = {
   bundle: true,
   entryPoints: [entryFile],
-  // Treat all dependencies in package.json as externals to keep bundle size to a minimum
-  external: Object.keys(dependencies),
   logLevel: 'info',
   minify: true,
   sourcemap: true,
